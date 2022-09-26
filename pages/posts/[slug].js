aac36743d8df30de740452c99c1879642556dbd6
nciampelletti <natashat@shaw.ca>
2022-09-26 12:09:32 -0700

    first commit

---
+++ pages/posts/[slug].js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const PostDetailPage = () => {
+  return <div>PostDetails</div>
+}
+
+export default PostDetailPage
---
+++ pages/posts/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const AllPostsPage = () => {
+  return <div>index</div>
+}
+
+export default AllPostsPage
