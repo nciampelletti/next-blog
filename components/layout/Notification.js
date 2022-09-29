57d80cd1e639fa3562648a172b2902c0d96964cf
nciampelletti <natashat@shaw.ca>
2022-09-29 13:24:16 -0700

    final checkin

--- components/layout/Notification.js
+++ components/layout/Notification.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom"
-import classes from "./notification.module.css"
+import classes from "./Notification.module.css"
 
 function Notification(props) {
   const { title, message, status } = props
